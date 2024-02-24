9f94d349250b6929cb578f148de321562f66e9ab
Insurgentko <123051300+Insurgentko@users.noreply.github.com>
2024-02-24 13:53:31 +0200

    init codelanggrid fun, which create in last commin in main fun, to run it

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -42,7 +42,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    CodeLangGrid(
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
